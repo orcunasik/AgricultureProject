6cfc54cc9920f19d1a8bff353efe6eb7c725f3ec
Orçun AŞIK <asikorcun@gmail.com>
2022-08-20 22:16:20 +0300

    GenericRepository inserted and IGenericDal implemented.

---
+++ Agriculture.DataAccess/Concrete/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Agriculture.DataAccess.Abstract;
+using Agriculture.DataAccess.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.DataAccess.Concrete.Repository
+{
+    public class GenericRepository<TEntity> : IGenericDal<TEntity> where TEntity : class, new()
+    {
+        public void Delete(TEntity entity)
+        {
+            using var context = new AgricultureContext();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public TEntity GetById(int id)
+        {
+            using var context = new AgricultureContext();
+            return context.Set<TEntity>().Find(id);
+        }
+
+        public List<TEntity> GetListAll()
+        {
+            using var context = new AgricultureContext();
+            return context.Set<TEntity>().ToList();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            using var context = new AgricultureContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            using var context = new AgricultureContext();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
