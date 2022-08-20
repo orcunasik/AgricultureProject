0e2f035b1b203aeba1bc3de2d55eb99143bb0e4c
Orçun AŞIK <asikorcun@gmail.com>
2022-08-20 21:57:41 +0300

    GenericDal and other interface configurations have been created.

---
+++ Agriculture.DataAccess/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.DataAccess.Abstract
+{
+    public interface IGenericDal<TEntity> where TEntity : class, new()
+    {
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        TEntity GetById(int id);
+        List<TEntity> GetListAll();
+    }
+}
