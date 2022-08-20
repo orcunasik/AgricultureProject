029f8ac3a159005e9d15fa0cf7682baa666a4a1d
Orçun AŞIK <asikorcun@gmail.com>
2022-08-20 19:58:32 +0300

    Context object and created migraiton for database.

---
+++ Agriculture.DataAccess/Contexts/AgricultureContext.cs
@@ -0,0 +1,24 @@
+﻿using Agriculture.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.DataAccess.Contexts
+{
+    public class AgricultureContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-QEHR73R\\SQLEXPRESS;database=Agriculture;integrated security=true");
+        }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
