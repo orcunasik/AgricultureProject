ad9f2e4f71cbf684005e42d39c36c0cd7ed2b64d
Orçun AŞIK <asikorcun@gmail.com>
2022-08-20 23:10:52 +0300

    Added "ServiceManager " class.

---
+++ Agriculture.Business/Concrete/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using Agriculture.Business.Abstract;
+using Agriculture.DataAccess.Abstract;
+using Agriculture.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Business.Concrete
+{
+    public class ServiceManager : IServiceService
+    {
+        private readonly IServiceDal _serviceDal;
+
+        public ServiceManager(IServiceDal serviceDal)
+        {
+            _serviceDal = serviceDal;
+        }
+
+        public void Delete(Service service)
+        {
+            _serviceDal.Delete(service);
+        }
+
+        public Service GetById(int id)
+        {
+            return _serviceDal.GetById(id);
+        }
+
+        public List<Service> GetListAll()
+        {
+            return _serviceDal.GetListAll();
+        }
+
+        public void Insert(Service service)
+        {
+            _serviceDal.Insert(service);
+        }
+
+        public void Update(Service service)
+        {
+            _serviceDal.Update(service);
+        }
+    }
+}
