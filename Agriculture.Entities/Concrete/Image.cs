03c2c20d6d1629d23d8e33ecb0d53c1b124450cc
Orçun AŞIK <asikorcun@gmail.com>
2022-08-20 19:27:32 +0300

    Add project files.

---
+++ Agriculture.Entities/Concrete/Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Entities.Concrete
+{
+    public class Image
+    {
+    }
+}
