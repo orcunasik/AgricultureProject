fe3fa25ec6744930a8453e4610cbbbb4f7f9230c
Orçun AŞIK <asikorcun@gmail.com>
2022-08-20 22:52:51 +0300

    Created EmployeeManager.

---
+++ Agriculture.Business/Concrete/EmployeeManager.cs
@@ -0,0 +1,46 @@
+﻿using Agriculture.Business.Abstract;
+using Agriculture.DataAccess.Abstract;
+using Agriculture.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Business.Concrete
+{
+    public class EmployeeManager : IEmployeeService
+    {
+        private readonly IEmployeeDal _employeeDal;
+
+        public EmployeeManager(IEmployeeDal employeeDal)
+        {
+            _employeeDal = employeeDal;
+        }
+
+        public void Delete(Employee employee)
+        {
+            _employeeDal.Delete(employee);
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employeeDal.GetById(id);
+        }
+
+        public List<Employee> GetListAll()
+        {
+            return _employeeDal.GetListAll();
+        }
+
+        public void Insert(Employee employee)
+        {
+            _employeeDal.Insert(employee);
+        }
+
+        public void Update(Employee employee)
+        {
+            _employeeDal.Update(employee);
+        }
+    }
+}
