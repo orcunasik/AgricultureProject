594e16c202ea9c94c5c5c12f66b96d4513545991
Orçun AŞIK <asikorcun@gmail.com>
2022-08-21 19:52:39 +0300

    ServiceAddViewModel added

---
+++ Agriculture.WebUI/Models/ServiceAddViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agriculture.WebUI.Models
+{
+    public class ServiceAddViewModel
+    {
+        [Display(Name ="Başlık")]
+        [Required(ErrorMessage ="Başlık Boş Geçilemez")]
+        public int Title { get; set; }
+
+        [Display(Name = "Açıklama")]
+        public string Description { get; set; }
+
+        [Display(Name = "Görsel")]
+        [Required(ErrorMessage = "Hizmet Ait Görsel Adresi Giriniz")]
+        public string ImageUrl { get; set; }
+    }
+}
