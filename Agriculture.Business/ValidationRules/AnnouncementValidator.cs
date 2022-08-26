74d655c35d6e91456836f6ef033c0140cdb754d3
Orçun AŞIK <asikorcun@gmail.com>
2022-08-26 21:16:52 +0300

    AnnouncementValidator added

---
+++ Agriculture.Business/ValidationRules/AnnouncementValidator.cs
@@ -0,0 +1,20 @@
+﻿using Agriculture.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Business.ValidationRules
+{
+    public class AnnouncementValidator : AbstractValidator<Announcement>
+    {
+        public AnnouncementValidator()
+        {
+            RuleFor(a => a.Title).NotEmpty().WithMessage("Son Gelişmeler Başlık Alanı Boş Geçilemez!");
+            RuleFor(a => a.Description).NotEmpty().WithMessage("Son Gelişmeler Açıklamalar Boş Geçilemez!");
+            RuleFor(a => a.ImageUrl).NotEmpty().WithMessage("Son Gelişmeler Görsel Ekleyiniz!");
+        }
+    }
+}
