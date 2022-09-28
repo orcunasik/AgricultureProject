679880d063525c6a2fa3564ec4633da02ce89b7d
Orçun AŞIK <asikorcun@gmail.com>
2022-09-28 16:40:04 +0300

    ImageManager created

---
+++ Agriculture.Business/Concrete/ImageManager.cs
@@ -0,0 +1,46 @@
+﻿using Agriculture.Business.Abstract;
+using Agriculture.DataAccess.Abstract;
+using Agriculture.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Business.Concrete
+{
+    public class ImageManager : IImageService
+    {
+        private readonly IImageDal _imageDal;
+
+        public ImageManager(IImageDal imageDal)
+        {
+            _imageDal = imageDal;
+        }
+
+        public void Delete(Image image)
+        {
+            _imageDal.Delete(image);
+        }
+
+        public Image GetById(int id)
+        {
+            return _imageDal.GetById(id);
+        }
+
+        public List<Image> GetListAll()
+        {
+            return _imageDal.GetListAll();
+        }
+
+        public void Insert(Image image)
+        {
+            _imageDal.Insert(image);
+        }
+
+        public void Update(Image image)
+        {
+            _imageDal.Update(image);
+        }
+    }
+}
