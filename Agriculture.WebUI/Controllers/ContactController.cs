835d7cf1b88e87e7e71c9c821f65220e42e35f16
Orçun AŞIK <asikorcun@gmail.com>
2022-10-11 14:19:21 +0300

    ContactController added

---
+++ Agriculture.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Agriculture.Business.Abstract;
+using Agriculture.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agriculture.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult Index()
+        {
+            var contactList = _contactService.GetListAll();
+            return View(contactList);
+        }
+        [HttpGet]
+        public IActionResult MessageDetails(int id)
+        {
+            var result = _contactService.GetById(id);
+            return View(result);
+        }
+        public IActionResult DeleteMessage(int id)
+        {
+            var result = _contactService.GetById(id);
+            _contactService.Delete(result);
+            return RedirectToAction("Index");
+        }
+    }
+}
