e7e2edab08ce262f7153c6986b28e698f39963ce
Orçun AŞIK <asikorcun@gmail.com>
2022-10-11 14:18:44 +0300

    ContactManager created

---
+++ Agriculture.Business/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using Agriculture.Business.Abstract;
+using Agriculture.DataAccess.Abstract;
+using Agriculture.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Business.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void Delete(Contact contact)
+        {
+            _contactDal.Delete(contact);
+        }
+
+        public Contact GetById(int id)
+        {
+            return _contactDal.GetById(id);
+        }
+
+        public List<Contact> GetListAll()
+        {
+            return _contactDal.GetListAll();
+        }
+
+        public void Insert(Contact contact)
+        {
+            _contactDal.Insert(contact);
+        }
+
+        public void Update(Contact contact)
+        {
+            _contactDal.Update(contact);
+        }
+    }
+}
