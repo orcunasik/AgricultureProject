c4131d370e593677880897539560d4d3ddeccfd6
Orçun AŞIK <asikorcun@gmail.com>
2022-10-21 17:05:41 +0300

    Added About and SocialMedia entities.

---
+++ Agriculture.Entities/Concrete/About.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture.Entities.Concrete
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string History { get; set; }
+        public string AboutUs { get; set; }
+    }
+}
