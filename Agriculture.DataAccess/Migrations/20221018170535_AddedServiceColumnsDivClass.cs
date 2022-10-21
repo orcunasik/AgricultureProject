92be262fa0647bd21eb23d02bf971e507839d13c
Orçun AŞIK <asikorcun@gmail.com>
2022-10-21 17:16:51 +0300

    Created Migration for About, SocialMedia and Admin.

---
+++ Agriculture.DataAccess/Migrations/20221018170535_AddedServiceColumnsDivClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agriculture.DataAccess.Migrations
+{
+    public partial class AddedServiceColumnsDivClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DivClass1",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DivClass2",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DivClass1",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "DivClass2",
+                table: "Services");
+        }
+    }
+}
