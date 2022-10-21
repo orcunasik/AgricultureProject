3d37cd95cbade26be66286065463864a06547176
Orçun AŞIK <asikorcun@gmail.com>
2022-10-21 17:13:36 +0300

    Recently added About, SocialMedia and Admin entities are linked to Context file.

--- Agriculture.DataAccess/Contexts/AgricultureContext.cs
+++ Agriculture.DataAccess/Contexts/AgricultureContext.cs
@@ -20,5 +20,8 @@ namespace Agriculture.DataAccess.Contexts
         public DbSet<Image> Images { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Admin> Admins { get; set; }
     }
 }
